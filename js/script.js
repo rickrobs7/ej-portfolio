f12d50a3bb3c666111ec97089cb7c8323519dbbb
Rick Robertson <rickrobs7@gmail.com>
2022-01-19 16:07:39 -0700

    Created new portfolio.

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$("document").ready(function() {
+  $(".project-name[data-type='javascript']").css("background-color", "#5b6ccf");
+  $(".project-name[data-type='htmlcss']").css("background-color", "#2f6e3f");
+  $(".project-name[data-type='game']").css("background-color", "#602c70");
+
+  document.querySelector("#html-css").addEventListener("change", function(evt) {
+    updateProjectView("htmlcsss", evt.target.checked);
+  });
+
+  document
+    .querySelector("#javascript")
+    .addEventListener("change", function(evt) {
+      updateProjectView("javascript", evt.target.checked);
+    });
+
+  document.querySelector("#games").addEventListener("change", function(evt) {
+    updateProjectView("game", evt.target.checked);
+  });
+
+  $(".project-item").each(function() {
+    var prodName = encodeURIComponent(
+      $(this)
+        .children("h2")
+        .text()
+    );
+    var prodID = encodeURIComponent($(this).data("prod_id"));
+
+    var link = $(
+      "<a href='project.html?prodName=" + prodName + "&prodID=" + prodID + "'/>"
+    );
+    $(this)
+      .children("img")
+      .wrap(link);
+  });
+});
+
+function updateProjectView(categoryName, bVisible) {
+  var dataSelectorVal = "";
+  switch (categoryName) {
+    case "htmlcsss":
+      dataSelectorVal = "h2[data-type='htmlcss']";
+      break;
+    case "javascript":
+      dataSelectorVal = "h2[data-type='javascript']";
+      break;
+    case "game":
+      dataSelectorVal = "h2[data-type='game']";
+      break;
+  }
+  $(".project-item")
+    .has(dataSelectorVal)
+    .css("display", bVisible ? "" : "none");
+}
